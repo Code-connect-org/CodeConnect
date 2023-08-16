26f1a6b6aa60958698fe7005b6a48bf7743fc891
YukkuriPannda <100471518+YukkuriPannda@users.noreply.github.com>
2023-08-16 23:12:21 +0900

    Add fireBullet

--- Assets/Script/Magics/BulletMagicController.cs
+++ Assets/Script/Magics/BulletMagicController.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class IceNeedleController : MonoBehaviour
+public class BulletMagicController : MonoBehaviour
 {
     public GameObject iceNeedle;
+    public float delay;
     // Start is called before the first frame update
     IEnumerator Start()
     {
-        yield return new WaitForSeconds(0.2f);
+        yield return new WaitForSeconds(delay);
         iceNeedle.SetActive(true);
         yield break;
     }
