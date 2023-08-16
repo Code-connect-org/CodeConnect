5f76a45f15195ed77f92a7c5c7d54019e536512f
YukkuriPannda <100471518+YukkuriPannda@users.noreply.github.com>
2023-08-16 12:28:15 +0900

    Update Graphic and effect and node and etc...
    mazi gomen commit sitenakatta wa

---
+++ Assets/Script/Node.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class Node : MonoBehaviour
+{
+    public NodeData nodeData;
+    [SerializeField]private TextMeshProUGUI text;
+    void Start()
+    {
+        text.text = nodeData.name;
+    }
+
+    public IEnumerator AttackStart(){
+        RectTransform rectTrf = gameObject.GetComponent<RectTransform>();
+        text.color = Color.white;
+        text.outlineWidth = 0;
+        for(float t = 0;t <= 0.25f;t += Time.deltaTime){
+            rectTrf.localScale = Vector3.one * (t * 0.8f + 1.0f);
+            yield return null;
+        }
+        yield return new WaitForSeconds(0.1f * nodeData.frame-0.25f);
+        text.color = new Color(0.1f,0.1f,0.1f);
+        text.outlineWidth = 1;
+        for(float t = 0;t <= 0.25f;t += Time.deltaTime){
+            rectTrf.localScale = Vector3.one * (t * -0.8f + 1.2f);
+            yield return null;
+        }
+        yield break;
+    }
+}
---
+++ Assets/Script/NodeManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEngine;
+using UnityEngine.Timeline;
+
+public class NodeManager : MonoBehaviour
+{
+    public List<NodeData> nodeDatas = new List<NodeData>();
+    public List<Node> nodes = new List<Node>();
+    public GameObject nodePrefab;
+    public Animator plAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitializeNodes();
+    }
+    public void InitializeNodes(){
+        for(int i =0;i < transform.childCount;i ++){
+            Destroy(transform.GetChild(i).gameObject);
+        }
+        nodes = new List<Node>();
+        for(int i = 0;i < nodeDatas.Count;i++){
+            nodes.Add(Instantiate(nodePrefab,transform).GetComponent<Node>());
+            nodes[i].nodeData = nodeDatas[i];
+        }
+    }
+    public IEnumerator Attack(){
+        InitializeNodes();
+        foreach(Node node in nodes){
+            plAnimator.Play(node.nodeData.name);
+            Destroy(Instantiate((GameObject)Resources.Load(node.nodeData.name),plAnimator.transform.position,Quaternion.identity),1.0f);
+            yield return StartCoroutine(node.AttackStart());
+        }
+    }
+}
+[System.Serializable]
+public class NodeData
+{
+    public string name;
+    public string code;//内部のコードのフレーバー部分
+    public int element;//0〜4の数値が入る
+    public int power; //100で攻撃そのまま程度
+    public int ap; //プレイヤーの基本値は100なので、20程度を想定
+    public int frame;
+    public int id;
+    public int skill_id;//特殊効果枠
+    public int heat;//オーバーヒートゲージの増加量
+    public float fail;
+}
